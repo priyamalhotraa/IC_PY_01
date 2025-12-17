e01551aa4bdc7b8b59019e883e68d6e10dfb47a1
priyamalhotraa <priyamalhotra14012005@gmail.com>
2025-12-17 19:14:15 +0530

    Add Python Console-Based Calculator solution
    This commit adds the Python implementation of the Console-Based Calculator
    for Task 1 assigned by Interncred.

---
+++ calculator.py
@@ -0,0 +1,23 @@
+#calculator program
+
+num1=float(input("Enter the first number: "))
+operator=input("Enter the operator (+,-,*,/,^,√,%) : ")
+num2=float(input("Enter the second number: "))
+
+if operator=='+':
+    print(num1,"+",num2, "=" ,num1+num2)
+elif operator=='-':
+    print(num1,"-",num2, "=" ,num1-num2)
+elif operator=='*':
+    print(num1,"*",num2,"=" ,num1*num2)
+elif operator=="/":
+    if num2==0 :
+        print("error!")
+    else:
+        print(num1,"/",num2, "=" ,num1/num2)
+elif operator=="^":
+    print(num1,"^",num2,"=" ,num1**num2)
+elif operator=='√':
+    print("The square root of ",num1, "is",num1**0.5)
+elif operator=='%':
+    print(num1,"%",num2, "=" ,(num1/100)*num2)
